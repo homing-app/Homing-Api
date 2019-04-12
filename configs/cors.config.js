4f42763373c12bf21a7ac8cfb0ff4babc31bee15
Roberto <roberto.m.aragon@gmail.com>
2019-04-12 20:15:08 +0200

    cors

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -1,15 +1,15 @@
 const createError = require('http-errors');
 const cors = require('cors');
 
-const allowedOrigins = process.env.CORS_ORIGINS || ['http://localhost:3000']
+const allowedOrigins = [process.env.CORS_ORIGINS || 'http://localhost:3000']
 
 module.exports = cors({
   origin: (origin, next) => {
-    const isAllowed = !origin || allowedOrigins.some(o => o === origin);
-    if(isAllowed) {
-      next(null, isAllowed)
+    const allowed = !origin || allowedOrigins.indexOf(origin) !== -1;
+    if (allowed) {
+      next(null, allowed)
     } else {
-      next(createError(401, "Not allowed by CORS"))
+      next(createError(401, 'ESTÁ LLEGANDO AQUÍ: not allowed by Cors'))
     }
   },
   credentials: true
