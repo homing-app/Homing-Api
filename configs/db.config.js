a7ae3204c49ca51e6b86f40dd0d40e6941daa4ec
Roberto <roberto.m.aragon@gmail.com>
2019-04-13 00:17:53 +0200

    i cant believe it

--- configs/db.config.js
+++ configs/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const DB_NAME = 'homing-api'
 // const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/${DB_NAME}`;
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = `mongodb+srv://robertusopo:${process.env.PASSWORD}@cluster0-0nnj0.mongodb.net/test`
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true})
   .then(() => {
